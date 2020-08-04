ad7cf82a34bed69d3a2d795e4250de933e582264
kalmax-sousa <kalmax.santos.123@gmail.com>
2020-08-04 16:36:10 -0300

    watchButton confiigurado

--- src/components/BannerMain/styles.jsx
+++ src/components/BannerMain/styles.jsx
@@ -100,7 +100,7 @@ export const BannerMainContainer = styled.section`
   }
 `;
 
-export const WatchButton = styled.button`
+export const WatchButton = styled.a`
   font-family: 'Roboto', sans-serif;
   box-sizing: border-box;
   cursor: pointer;
@@ -110,17 +110,17 @@ export const WatchButton = styled.button`
   font-size: 16px;
   outline: none;
   border-radius: 5px;
+  text-align: center;
   text-decoration: none;
   display: inline-block;
-  border: 1px solid transparent;
-  color: #000000;
-  background: var(--white);
-  border-color: var(--black);
+  color: var(--white);
+  background: var(--primary);
   transition: opacity .3s;
   display: none;
   margin: 0 auto;
   
   @media (max-width: 800px) {
     display: block;
+    max-width: 150px;
   }
 `;
